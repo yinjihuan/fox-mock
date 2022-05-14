d4f93f65e114b7d2669af9d2ea6adaf1fe4d612e
jihuan.yin <jihuan900@126.com>
2022-05-14 23:03:20 +0800

    去掉agent中mybatis依赖，最小化方式实现

---
+++ fox-mock-agent/src/main/java/org/apache/ibatis/mapping/MappedStatement.java
@@ -0,0 +1,15 @@
+package org.apache.ibatis.mapping;
+
+
+public final class MappedStatement {
+
+    private String id;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
